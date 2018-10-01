647d35b1e3c06dd259f1641c64f414dad25bce87
mwinata94 <mwinata@mail.sfsu.edu>
2018-10-01 13:14:19 -0700

    Complete method definition

--- HW-2/1.cpp
+++ HW-2/1.cpp
@@ -8,5 +8,27 @@ int main() {
 }
 
 int maxlen(int array[], int size) {
-	return 0;
+	if ( 0 == size ) {
+		return size;
+	} else {
+		int integer = array[0];
+		int counter = 0;
+		int maxlen = 0;
+		for (int i = 0; i < size; i++) {
+			if ( integer == array[i] ) {
+				counter++;
+			} else { 
+				if ( maxlen < counter ) {
+					maxlen = counter;
+				}
+				counter = 1;
+				integer = array[i];
+			}
+		}
+		if ( maxlen < counter ) {
+			return counter;
+		} else {
+			return maxlen;
+		}
+	}
 }
