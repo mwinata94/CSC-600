9e3f8121aafdde8fce25f3a01d0a04897264702c
mwinata94 <mwinata@mail.sfsu.edu>
2018-10-01 13:17:12 -0700

    Finish user input

--- HW-2/1.cpp
+++ HW-2/1.cpp
@@ -4,6 +4,17 @@ using namespace std;
 int maxlen(int[], int);
 
 int main() {
+	int size = 0;
+	int* array;
+	cout << "Enter array size: ";
+	cin >> size;
+	array = new int[size];
+	for (int i = 0; i < size; i++) {
+		cout << "Enter element #" << i + 1 << ":";
+		cin >> array[i];
+	}
+	cout << "maxlen: " << maxlen(array, size) << endl;
+	delete array;
 	return 0;
 }
 
