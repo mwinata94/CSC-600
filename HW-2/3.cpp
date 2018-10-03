7dc699a75842822d02f97bc25370255a6f0ab2b9
mwinata94 <mwinata@mail.sfsu.edu>
2018-10-02 21:04:37 -0700

    Finish user input and store array size into the first element

--- HW-2/3.cpp
+++ HW-2/3.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int* reduce(int[], int);
 
 int main() {
+    int size;
+    do {
+        cout << "Enter array size (must be positive): ";
+        cin >> size;
+    } while (size < 1);
+    int array[size];
+    for (int i = 0; i < size; i++) {
+        cout << "Enter element #" << i + 1 << ": ";
+        cin >> array[i];
+    }
+    int* newArray = reduce(array, size);
+    cout << "Reduced array:" << endl << "[ ";
+    int newSize = newArray[0];
+    for (int i = 0; i < newSize; i++) {
+        cout << newArray[i + 1] << ", ";
+    }
+    if (0 == newSize) {
+        cout << "]";
+    } else {
+        cout << "\b\b ]";
+    }
 	return 0;
 }
 
@@ -42,8 +63,9 @@ int* reduce(int array[], int size) {
             thirdCount++;
         }
     }
-    int returnArray[size - firstCount - secondCount - thirdCount];
-    for (int i(0), j(0); i < size; i++) {
+    int* returnArray = new int[size - firstCount - secondCount - thirdCount + 1];
+    returnArray[0] = size - firstCount - secondCount - thirdCount;
+    for (int i(0), j(1); i < size; i++) {
         if (array[i] < thirdLargest) {
             returnArray[j++] = array[i];
         }
