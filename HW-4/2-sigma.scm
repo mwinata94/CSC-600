b310d7289bea0b2a478b1582d7ce41c83cf493df
Michael Winata <32146559+mwinata94@users.noreply.github.com>
2018-11-28 16:16:35 -0800

    clean up

--- HW-4/2-sigma.scm
+++ HW-4/2-sigma.scm
@@ -6,33 +6,37 @@
       ( + x ( apply sum y ) )
   )
 )
+
 ( define ( square_sum x . y )
   ( if ( null? y )
     ( square x )
     ( + ( square x ) ( apply square_sum y ) )
   )
 )
+
 ( define ( length x . y ) 
   ( if ( null? y )
     1
     ( + 1 ( apply length y ) )
   )
 )
+
 ( define ( mean x . y )
   ( / 
     ( apply sum ( cons x y ) ) 
     ( apply length ( cons x y ) ) 
   )
 )
+
 ( define ( square_mean x . y )
   ( / 
     ( apply square_sum ( cons x y ) ) 
     ( apply length ( cons x y ) ) 
   )
 )
+
 ( define ( sigma x . y )
-  ( sqrt (
-    -
+  ( sqrt ( -
     ( apply square_mean ( cons x y ) )
     ( square ( apply mean ( cons x y ) ) )
   ) )
